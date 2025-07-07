cc3464eb778e41dff4017378db13ff72663e2359
Christopher-walsh22 <106549296+Christopher-walsh22@users.noreply.github.com>
2025-07-07 17:30:18 +0000

    Update test to include config for login

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,22 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
-
+import { ConfigService } from '../services/config.service';
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HomeComponent]
-    })
-    .compileComponents();
+  await TestBed.configureTestingModule({
+    imports: [HomeComponent],
+    providers: [
+      { provide: ConfigService, useValue: {} }
+    ]
+  })
+  .compileComponents();
 
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
+  fixture = TestBed.createComponent(HomeComponent);
+  component = fixture.componentInstance;
+  fixture.detectChanges();
+});
   it('should create', () => {
     expect(component).toBeTruthy();
   });
