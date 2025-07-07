8426d755a38b06bebce15acfeb7e43a4b4f6d2ce
cameron <cameron@digitalspace.ca>
2025-07-07 21:03:09 +0000

    one more test

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { ConfigService } from './config.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('AuthService', () => {
   let service: AuthService;
-
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [HttpClient, HttpHandler, ConfigService]
+    });
     service = TestBed.inject(AuthService);
   });
 
