73858a8b9b7757c711626420be5db4ce87e19545
Mark Lisé <mark@digitalspace.ca>
2025-09-05 15:06:43 -0700

    refactor: pass full process.env to stack, use dynamic resource names
    - Pass entire process.env to ReserveRecAdminStack instead of manually mapping env vars
    - Use dynamic resource names in CloudFront setup based on STACK_NAME from env
    - Add log for STACK_NAME in S3 setup

--- bin/reserve-rec-admin.js
+++ bin/reserve-rec-admin.js
@@ -5,14 +5,5 @@ const { ReserveRecAdminStack } = require('../lib/reserve-rec-admin-stack');
 
 const app = new cdk.App();
 new ReserveRecAdminStack(app, process.env.STACK_NAME, {
-  env: {
-    //AWS account variables
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-
-    // Custom environment variables
-    ENVIRONMENT: process.env.ENVIRONMENT,
-    S3_BUCKET: process.env.S3_BUCKET,
-    API_STAGE: process.env.API_STAGE,
-  }
+  env: process.env
 });
