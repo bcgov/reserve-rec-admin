664f810fdee9a18c3d388c17760026993cdbe88a
cameron <cameron@digitalspace.ca>
2025-10-23 20:49:20 +0000

    removing unnecessary bin file

--- bin/reserve-rec-admin.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-const cdk = require('aws-cdk-lib');
-const { ReserveRecAdminStack } = require('../lib/reserve-rec-admin-stack');
-
-const app = new cdk.App();
-new ReserveRecAdminStack(app, process.env.STACK_NAME, {
-  env: process.env
-});
